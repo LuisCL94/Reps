4541c8dfeb9c90f5b0d35193e9f671cd44894285
Luis Claudio Telles Lima <lclaudio.tl@gmail.com>
2020-01-02 02:33:48 -0300

    Connecting with apis and added animation on plus button

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,11 +1,47 @@
 import React, { Component } from 'react';
+import { FaGithubAlt, FaPlus, FaSpinner } from "react-icons/fa";
+
+import api from '../../services/api';
 
 import { Container, Form, SubmitButton } from "./styles";
-import { FaGithubAlt, FaPlus } from "react-icons/fa";
 
 export class Main extends Component {
+state = {
+    newRepo: '',
+    repositories: [],
+    loading: false,
+  }
+
+  handleInputChange = e => {
+    this.setState({ newRepo: e.target.value});
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault(); //evita que o formulario faca refresh na pagina
+  
+    this.setState({loading: true});
+
+    // const {newRepo, repositories} = this.state;
+
+    const response = await api.get(`/repos/${this.state.newRepo}`);
+  
+    // console.log(response.data);
+
+    const data = {
+      name: response.data.full_name,
+    }
+
+    this.setState({
+      repositories: [...this.state.repositories, data],
+      newRepo: '',
+      loading: false,
+    });
+
+  }
 
   render() {
+    // const { newRepo } = this.state;
+    
     return (
       <>
         <Container>
@@ -13,11 +49,18 @@ export class Main extends Component {
             <FaGithubAlt />
             Repositórios
         </h1>
-          <Form onSubmit={() => { }}>
-            <input type="text" placeholder="Adicionar repositório" />
-
-            <SubmitButton disable>
-              <FaPlus color="#FFF" size={14} />
+          <Form onSubmit={this.handleSubmit}>
+            <input 
+              type="text"
+              placeholder="Adicionar repositório" 
+              value = {this.state.newRepo} 
+              onChange = {this.handleInputChange}
+            />
+            <SubmitButton loading={this.state.loading}>
+              {this.state.loading ? (
+                <FaSpinner color="#FFF" size={14} /> 
+              ) : (
+                <FaPlus color="#FFF" size={14} />) }
             </SubmitButton>
           </Form>
         </Container>
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,4 +1,5 @@
-import styled from "styled-components";
+import styled, { keyframes, css } from "styled-components";
+import { animationFrame } from "rxjs/internal/scheduler/animationFrame";
 
 export const Container = styled.div`
   max-width: 700px;
@@ -34,9 +35,20 @@ export const Form = styled.form`
   }
 `;
 
-export const SubmitButton = styled.button.attrs({
-  type: "submit" //eu posso passa passar os atributos direto por aqui
-})`
+// criando funcao de animação de giro em css;
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg); /*item comeca sem nenhuma rotacao */
+
+  }  to {
+    transform: rotate(360deg);
+  }
+`;
+
+export const SubmitButton = styled.button.attrs(props => ({
+  type: "submit", //eu posso passa passar os atributos direto por aqui
+  disabled: props.loading
+}))`
   background: #7159c1;
   border: 0;
   padding: 0 15px;
@@ -46,138 +58,17 @@ export const SubmitButton = styled.button.attrs({
   display: flex;
   justify-content: center;
   align-items: center;
-`;
 
-export const Navbar = styled.ul`
-  list-style-type: none;
-  overflow: hidden;
-  background-color: #29088a;
-  display: flex;
-  justify-content: space-around;
-
-  li {
-    float: left;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-
-    svg {
-      margin-right: -20px;
-    }
+  &[disabled] {
+    cursor: not-allowed;
+    opacity: 0.6;
   }
-
-  h5,
-  h2 {
-    display: inline-block;
-    color: white;
-    /* text-align: center; */
-    padding: 25px;
-    text-decoration: none;
-  }
-
-  /* li a:hover,
-  .dropdown:hover .dropbtn {
-    background-color: red;
-  } */
-`;
-
-export const Boxes = styled.div`
-  justify-content: center;
-
-  padding-left: 5px;
-  padding-right: 5px;
-
-  display: flex;
-  flex-wrap: wrap;
-
-  div {
-    max-width: 215px;
-    padding: 70px;
-
-    margin: 15px;
-    padding-bottom: 10px;
-
-    background: #fff;
-    border-radius: 10px;
-
-    box-shadow: 0 0 20px rgba(0, 0, 0, 0.5);
-
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-
-    img {
-      width: 190px;
-      height: 190px;
-      margin-top: -70px;
-    }
-
-    h5 {
-      font-size: 17px;
-      color: #473eac;
-    }
-    h2 {
-      margin-top: 15px;
-      width: 150px;
-      text-align: center;
-    }
-
-    button {
-      /* color: #fff !important; */
-      /* text-transform: uppercase; */
-      /* text-decoration: none; */
-      background: #473eac;
-      padding: 10px;
-      border-radius: 20px;
-      width: 200%;
-      color: white;
-      margin-top: 20px; /*aqui aumenta verticalmente pra baixo */
-      margin-left: -30px;
-      /* display: inline-block; */
-      /* border: none; */
-      /* transition: all 0.4s ease 0s; */
-    }
-  }
-`;
-
-export const Tabs = styled.ul`
-  list-style-type: none;
-  margin: auto;
-  margin-top: 30px;
-
-  max-width: auto;
-  background: #000;
-
-  display: flex;
-  justify-content: center;
-  border-radius: 10px;
-  padding: 2px 2px;
-
-  li {
-    /* float: left; */
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-  }
-
-  svg {
-    margin-right: 10px;
-  }
-
-  button {
-    color: #7159c1;
-    background: black;
-    text-align: center;
-    padding: 15px 15px;
-    border: 1px solid black;
-
-    margin: auto 30px;
-  }
-
-  button:hover {
-    /* background: #fff; */
-    color: orange;
-    border-radius: 10px;
-    border: 1px solid orange;
-  }
+  
+  ${props=> 
+    props.loading &&
+    css`
+      svg {
+        animation: ${rotate} 2s linear infinite;
+      }
+    `}
 `;
